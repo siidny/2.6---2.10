4f569be6b47674bf46221ec1e0e98b9a4d66d639
josh-ga <josh.armistead@det.nsw.edu.au>
2023-02-23 23:35:02 +1100

    tailwind add/remove

--- src/App.js
+++ src/App.js
@@ -2,10 +2,14 @@ import { useState } from "react";
 
 const App = () => {
   const [persons, setPersons] = useState([
-    { id: 1, name: "Arto Hellas", number: 613956 },
+    { name: "Arto Hellas", number: "040-123456", id: 1 },
+    { name: "Ada Lovelace", number: "39-44-5323523", id: 2 },
+    { name: "Dan Abramov", number: "12-43-234345", id: 3 },
+    { name: "Mary Poppendieck", number: "39-23-6423122", id: 4 },
   ]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
+  const [filterText, setFilterText] = useState("");
 
   const addName = (event) => {
     event.preventDefault();
@@ -29,10 +33,19 @@ const App = () => {
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
   };
+  const handleFilterChange = (event) => {
+    setFilterText(event.target.value);
+  };
+  const filteredPersons = persons.filter(({ name }) =>
+    name.toLowerCase().includes(filterText.toLowerCase())
+  );
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+        filter: <input value={filterText} onChange={handleFilterChange} />
+      </div>
       <form onSubmit={addName}>
         <div>
           name: <input value={newName} onChange={handleNameChange} />
@@ -44,8 +57,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-        {persons.map(({ name, number }) => (
-          <li key={name.id}>
+        {filteredPersons.map(({ name, number, id }) => (
+          <li key={id}>
             {name}: {number}
           </li>
         ))}
